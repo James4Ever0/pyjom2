29c856e7c8ce25dc17a954d12b40b016bb02add9
GitJournal <app@gitjournal.io>
2024-02-20 01:31:49 +0800

    update

---
+++ project_management_and_credit_system/main.py
@@ -0,0 +1,6 @@
+# syntax:
+
+# pyjom register_video --project_root jianying --video x.mp4 --project_name x --hosted_platform douyin --hosted_url https://...
+# pyjom project_root_alias --project_root ... --alias jianying
+
+# monitor via hosted url.
