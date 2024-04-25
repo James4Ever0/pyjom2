28a5b768ed5b280ddf56f2f361b83da5c514f528
Your Name <you@example.com>
2024-04-26 07:24:36 +0800

    update

--- scripts/run_query_bilibili_video_count.sh
+++ scripts/run_query_bilibili_video_count.sh
@@ -1,1 +1,5 @@
 curl "http://localhost:8042/query/current_view_count?vid=BV1Qt4y1k7cK&platform=bilibili"
+# {"view_count":15.0,"timestamp":1713956653.893083}
+echo
+curl "http://localhost:8042/query/history_view_count?vid=BV1Qt4y1k7cK&platform=bilibili"
+echo
--- scripts/run_record_server.sh
+++ scripts/run_record_server.sh
@@ -1,2 +1,5 @@
 cd ../project_management_and_credit_system
-env SYNC_TIME=1 python record.py
+python record.py
+# env SYNC_TIME=1 python record.py
+# sudo: The "no new privileges" flag is set, which prevents sudo from running as root.
+# sudo: If sudo is running in a container, you may need to adjust the container configuration to disable the flag.
